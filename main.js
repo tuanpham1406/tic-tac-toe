36c30038d2d096e4c8aea4b36646d8bf5c8abb23
tuanpham1406 <phamanhtuan1406@gmail.com>
2019-04-10 14:49:24 +0700

    mess

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let b =document.getElementById("carogame");
 let board = [];
 let data = "";
 for (let i = 0; i < 5; i++){
-    board[i] = new Array(0,0,0,0,0);
+    board[i] = new Array(".", ".", ".", ".", ".");
 }
 for (let i = 0; i < 5; i++){
     data += "<br/>";
@@ -13,11 +13,20 @@ for (let i = 0; i < 5; i++){
 }
 data += "<br/><br/><input type='button' value='Change Value' onclick='changeValue()'>";
 b.innerHTML = data;
+
 function changeValue() {
+    let player = prompt("Player : ");
     let positionX = parseInt(prompt("X: "));
     let positionY = parseInt(prompt("Y: "));
     data = "";
-    board[positionX][positionY] = "x";
+    if(player == "A"){
+        board[positionX][positionY] = "x";
+    } else if (player == "B") {
+        board[positionX][positionY] = "o";
+    } else {
+        alert("Moi ban nhap A hoac B!!!");
+    }
+
     for (let i = 0; i < 5; i++) {
         data += "<br/>";
         for (let j = 0; j < 5; j++) {
